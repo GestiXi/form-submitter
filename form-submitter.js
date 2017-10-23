a5195654646caee5e19c4d82140e86b91b552c58
Nicolas Badia <badia.nicolas@gmail.com>
2017-10-23 08:17:14 +0200

    Updates to version 1.1

--- form-submitter.js
+++ form-submitter.js
@@ -3,7 +3,7 @@
 // Description: jQuery plugin used in GestiXi websites to handle form submission
 // Copyright:   ©2013-2016 GestiXi
 // License:     Licensed under the MIT license (see LICENCE)
-// Version:     1.0
+// Version:     1.1
 // Author:      Nicolas BADIA
 // ==========================================================================
 
